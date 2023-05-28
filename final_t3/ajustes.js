55d023c7cf901daf2e5efb367149a68288082a3d
Ricardojdc <ricardo.rjdc@gmail.com>
2023-05-28 18:40:09 +0200

    asd

--- final_t3/ajustes.js
+++ final_t3/ajustes.js
@@ -1,4 +1,4 @@
-// Check if there are any saved preferences in web storage
+
 var fontSize = localStorage.getItem('fontSize');
 var backgroundColor = localStorage.getItem('backgroundColor');
 
@@ -10,7 +10,6 @@ if (backgroundColor) {
   document.body.style.backgroundColor = backgroundColor;
 }
 
-// Function to increase the font size
 function increaseFontSize() {
   var currentSize = parseInt(getComputedStyle(document.body).fontSize);
   var newSize = currentSize + 2 + 'px';
@@ -18,7 +17,7 @@ function increaseFontSize() {
   localStorage.setItem('fontSize', newSize);
 }
 
-// Function to decrease the font size
+
 function decreaseFontSize() {
   var currentSize = parseInt(getComputedStyle(document.body).fontSize);
   var newSize = currentSize - 2 + 'px';
@@ -26,7 +25,6 @@ function decreaseFontSize() {
   localStorage.setItem('fontSize', newSize);
 }
 
-// Function to change the background color
 function changeBackgroundColor() {
   var colorInput = document.getElementById('colorInput');
   var color = colorInput.value;
@@ -36,34 +34,35 @@ function changeBackgroundColor() {
 
 
 /**********************************************************************/ 
-const formUsuario = document.getElementById('formUsuario');
+const formUsuario = document.getElementById('form');
 const nombreUsuarioDiv = document.getElementById('nombreusuario');
 
-// Comprobar si hay un nombre guardado en la memoria de sesión
+
 const nombreGuardado = sessionStorage.getItem('nombreUsuario');
 if (nombreGuardado) {
-  // Mostrar el nombre guardado en el div
+
   nombreUsuarioDiv.textContent = nombreGuardado;
+  
 } else {
-  // Mostrar "Sin identificar" si no hay nombre guardado
+
   nombreUsuarioDiv.textContent = 'Sin identificar';
 }
 
-// Agregar un evento de escucha al formulario cuando se envíe
-formUsuario.addEventListener('submit', function (event) {
-  event.preventDefault(); // Evitar el envío del formulario
 
-  // Obtener el valor del nombre introducido por el usuario
+formUsuario.addEventListener('submit', function (event) {
+  event.preventDefault(); 
+
+
   const nombreUsuario = document.getElementById('nombreInput').value;
 
-  // Guardar el nombre en la memoria de sesión
+  
   sessionStorage.setItem('nombreUsuario', nombreUsuario);
 
-  // Mostrar el nombre en el div
+ 
   nombreUsuarioDiv.textContent = nombreUsuario;
 
   console.log(nombreUsuario);
 
-  // Reiniciar el formulario
+
   formUsuario.reset();
 });
